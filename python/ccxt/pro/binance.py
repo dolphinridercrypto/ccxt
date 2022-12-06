1fb1ef45c8951305b039e0f4b0aa3b7a710fd397
dolphinridercrypto <cryptodolphinrider@gmail.com>
2022-12-06 02:47:51 -0700

    Test

--- python/ccxt/pro/binance.py
+++ python/ccxt/pro/binance.py
@@ -36,8 +36,8 @@ class binance(Exchange, ccxt.async_support.binance):
                 },
                 'api': {
                     'ws': {
-                        'spot': 'wss://stream.binance.com:9443/ws',
-                        'margin': 'wss://stream.binance.com:9443/ws',
+                        'spot': 'wss://data-stream.binance.com/ws',
+                        'margin': 'wss://data-stream.binance.com/ws',
                         'future': 'wss://fstream.binance.com/ws',
                         'delivery': 'wss://dstream.binance.com/ws',
                     },
